0561f00f70db31ef61aa9542433f28de162828c0
Kylian LucasThuillier <kylian.lucast@gmail.com>
2025-03-31 13:52:43 +0200

    Page evenement en dur

--- app/repositories/EvenementRepository.php
+++ app/repositories/EvenementRepository.php
@@ -31,20 +31,58 @@ class EvenementRepository {
         return $stmt->execute($data);
 	}
 
-    public function update(array $data): bool {
-        $query = 'UPDATE evenement 
-                 SET titre = :titre, 
-                     description = :description, 
-                     date_evenement = :date_evenement,
-                     prix = :prix,
-                     max_participants = :max_participants,
-                     nb_inscrits = :nb_inscrits,
-                     chemin_image = :chemin_image
-                 WHERE id = :id';
-        
-        $stmt = $this->pdo->prepare($query);
-        return $stmt->execute($data);
-    }
+	public function findAll(): array
+	{
+		$query = $this->pdo->query('SELECT * FROM evenement');
+		$query->setFetchMode(PDO::FETCH_CLASS, Evenement::class);
+		return $query->fetchAll();
+	}
+
+	public function create(Evenement $evenement): bool {
+		$stmt = $this->pdo->prepare('
+		INSERT INTO evenement (name, description, date, image, place, price)
+		VALUES (:name, :description, :date, :image, :place, :price)
+	');
+
+		return $stmt->execute([
+			'name'        => $evenement->getName(),
+			'description' => $evenement->getDescription(),
+			'date'        => $evenement->getDate(),
+			'image'       => $evenement->getImage(),
+			'place'       => $evenement->getPlace(),
+			'price'       => $evenement->getPrice()
+		]);
+	}
+
+	public function delete(int $id): bool {
+		$stmt = $this->pdo->prepare('DELETE FROM evenement WHERE id = :id');
+		return $stmt->execute(['id' => $id]);
+	}
+	
+	public function update(Evenement $evenement): bool {
+		$stmt = $this->pdo->prepare('
+			UPDATE evenement
+			SET name = :name, description = :description, date = :date, image = :image, place = :place, price = :price
+			WHERE id = :id
+		');
+
+		return $stmt->execute([
+			'id' => $evenement->getId(),
+			'name' => $evenement->getName(),
+			'description' => $evenement->getDescription(),
+			'date' => $evenement->getDate(),
+			'image' => $evenement->getImage(),
+			'place' => $evenement->getPlace(),
+			'price' => $evenement->getPrice()
+		]);
+	}
+
+	private function createEvenementFromRow(array $row): Evenement
+	{
+		return new Evenement($row['id'], $row['name'], $row['description'], $row['date'], $row['image'], $row['place'], $row['price']);
+	}
+
+}
 
     public function delete(int $id): bool {
         $query = 'DELETE FROM evenement WHERE id = :id';
