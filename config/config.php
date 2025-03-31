f293f92066f81c928d8f767581b0a75556a495b4
Nicolas CHANTEUX <nicolas.chanteux@etu.univ-lehavre.fr>
2025-03-31 10:37:58 +0200

    init repo

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+// Définition de constantes pour la configuration de la base de données
+define('DB_HOST', 'woody');  // Hôte de la base de données (généralement localhost)
+define('DB_NAME', 'cn230854');  // Nom de la base de données
+define('DB_USER', 'cn230854');  // Nom d'utilisateur de la base de données
+define('DB_PASS', '28022005');  // Mot de passe de la base de données
+define('DB_PORT', '5432');  // Mot de passe de la base de données
