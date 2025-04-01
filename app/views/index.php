61e3d32feb25cb498d5b61949b10dd0ea3b26a92
Rémi <remiviez@gmai.com>
2025-04-01 11:22:19 +0200

    event on accueil

--- app/views/index.php
+++ app/views/index.php
@@ -9,11 +9,7 @@ require_once 'app/views/template/header.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BDE IUT Informatique</title>
     <link rel="stylesheet" href="/assets/css/style.css">
-<<<<<<< HEAD
 	<link rel="stylesheet" href="/assets/css/accueil.css">
-=======
-    
->>>>>>> aa7de13 (tmp)
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
 <body>
