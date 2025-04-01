7586c953b25a7cbf9fbde61a6510750843e920fd
Rémi <remiviez@gmai.com>
2025-04-01 16:40:21 +0200

    css pour tous

--- app/views/template/header.php
+++ app/views/template/header.php
@@ -9,7 +9,6 @@ require_once 'app/middlewares/AuthMiddleware.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BDE IUT Informatique</title>
     <link rel="stylesheet" href="/assets/css/style.css">
-    <link rel="stylesheet" href="/assets/css/accueil.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
 <body>
