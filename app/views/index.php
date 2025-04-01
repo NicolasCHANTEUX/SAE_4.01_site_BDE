aa7de1327940752db2307e728eec02f8f1c33df9
Rémi <remiviez@gmai.com>
2025-04-01 11:01:52 +0200

    tmp

--- app/views/index.php
+++ app/views/index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BDE IUT Informatique</title>
     <link rel="stylesheet" href="/assets/css/style.css">
+    
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <script src="/assets/js/main.js" defer></script>
 </head>
@@ -76,7 +77,8 @@
             </section>
 
             <!-- Section des evenements -->
-            <section>
+            <section id="Events" class="event-container">
+
                 
 
             </section>
