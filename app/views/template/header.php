fede6f10aa00e299ae267fa7c68a48ece9b50e99
Rémi <remiviez@gmai.com>
2025-04-01 16:40:21 +0200

    css pour tous

--- app/views/template/header.php
+++ app/views/template/header.php
@@ -14,7 +14,6 @@ require_once 'app/middlewares/AuthMiddleware.php';
     <!-- Ajouter Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="/assets/css/style.css">
-    <link rel="stylesheet" href="/assets/css/accueil.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
 <body>
