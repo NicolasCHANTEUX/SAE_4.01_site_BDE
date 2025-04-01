4ae9e7a3b7bd97f17627948b70fbd9c0f63e9786
Rémi <remiviez@gmai.com>
2025-04-01 16:40:21 +0200

    css pour tous

--- assets/js/accueil.js
+++ assets/js/accueil.js
@@ -79,39 +79,3 @@ document.addEventListener('DOMContentLoaded', () => {
         voirPlusItem.style.transform = 'scale(1)';
     });
 });
-
-document.addEventListener("DOMContentLoaded", () => {
-    const menuToggle = document.getElementById("menu-toggle");
-    const mobileMenu = document.getElementById("mobile-menu");
-
-	// Ouvrir/fermer le menu déroulant
-	menuToggle.addEventListener("click", () => {
-		mobileMenu.classList.toggle("open");
-	});
-
-	// Mettre à jour le titre de la page actuelle
-	const currentPageTitle = document.getElementById("current-page-title");
-	const currentPath = window.location.pathname;
-
-	switch (currentPath) {
-		case "/":
-			currentPageTitle.textContent = "Accueil";
-			break;
-		case "/evenement.php":
-			currentPageTitle.textContent = "Événements";
-			break;
-		case "/boutique.php":
-			currentPageTitle.textContent = "Boutique";
-			break;
-		case "/contact.php":
-			currentPageTitle.textContent = "Contact";
-			break;
-		case "/connexion.php":
-			currentPageTitle.textContent = "Connexion";
-			break;
-		default:
-			currentPageTitle.textContent = "Page";
-			break;
-	}
-});
-
