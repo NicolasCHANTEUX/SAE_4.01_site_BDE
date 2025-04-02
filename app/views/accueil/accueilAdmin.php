f4d101babf37ae2dc6fcf4e399d52064340d3994
Kylian LucasThuillier <kylian.lucast@gmail.com>
2025-04-02 16:33:28 +0200

    getteur

--- app/views/accueil/accueilAdmin.php
+++ app/views/accueil/accueilAdmin.php
@@ -15,10 +15,10 @@ require_once 'app/views/template/header.php';
     <tbody>
     <?php foreach ($articles as $article): ?>
         <tr>
-            <td><?= $article['id'] ?></td>
-            <td><?= $article['titre'] ?></td>
-            <td><?= $article['description'] ?></td>
-            <td><?= $article['date_creation'] ?></td>
+            <td><?= $article->getId() ?></td>
+            <td><?= $article->getTitre() ?></td>
+            <td><?= $article->getDescription() ?></td>
+            <td><?= $article->getDateCreation() ?></td>
             <td>
                 <a href="article_update.php?id=<?= $article->getId() ?>" class="btn btn-warning btn-sm"><i class="fa fa-edit"></i></a>
                 <a href="article_delete.php?id=<?= $article->getId() ?>" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></a>
