ec913e14206c4025b95d247b7c8f38e4d6e97692
Nicolas CHANTEUX <nicolas.chanteux@etu.univ-lehavre.fr>
2025-04-02 15:31:18 +0200

    connexion fonctionnelle partiellement

--- connexion.php
+++ connexion.php
@@ -1,12 +1,5 @@
 <?php
 require_once './app/controllers/AuthController.php';
-require_once './app/middlewares/AuthMiddleware.php';
-
-// Rediriger si déjà connecté
-if (AuthMiddleware::isAuthenticated()) {
-    header('Location: /compte.php');
-    exit();
-}
 
 $controller = new AuthController();
 echo $controller->login();
