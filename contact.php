1d6a396afdd8c777d6a2e5770e66bb2f9470b4cb
Nicolas CHANTEUX <nicolas.chanteux@etu.univ-lehavre.fr>
2025-04-03 11:23:36 +0200

    page contact plus ergonomique + envoie formulaire contact MAIS NE FONCTIONNE PAS

--- contact.php
+++ contact.php
@@ -4,10 +4,8 @@ require_once __DIR__ . '/app/controllers/ContactController.php';
 
 $controller = new ContactController();
 
-// Vérifie si la requête est pour envoyer un message
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && strpos($_SERVER['REQUEST_URI'], '/contact/envoyer') !== false) {
     $controller->envoyerMessage();
 } else {
-    // Affichage normal de la page de contact
     $controller->index();
 }
