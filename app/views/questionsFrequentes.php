0dad32650a19ff41a5b4b31a6b4cea06809656ba
Nicolas CHANTEUX <nicolas.chanteux@etu.univ-lehavre.fr>
2025-04-03 12:47:06 +0200

    header faq

--- app/views/questionsFrequentes.php
+++ app/views/questionsFrequentes.php
@@ -1,37 +1,18 @@
+<?php
+require_once 'app/views/template/header.php';
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= htmlspecialchars($produit['nom'] ?? 'Produit non disponible') ?> - BDE IUT Informatique</title>
+    <title>BDE IUT Informatique</title>
     <link rel="stylesheet" href="/assets/css/style.css">
-    <link rel="stylesheet" href="/assets/css/questionsFrequentes.css">
+	<link rel="stylesheet" href="/assets/css/questionsFrequentes.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
 <body>
-<header>
-        <nav class="main-nav">
-            <div class="nav-brand">
-                <a href="/">
-                    <img src="/assets/images/logo.png" alt="Logo BDE" class="logo">
-                    <span>BDE Info</span>
-                </a>
-            </div>
-            <div class="nav-links">
-                <a href="/" class="active">Accueil</a>
-                <a href="/evenement.php">Événements</a>
-                <a href="/boutique.php">Boutique</a>
-                <a href="/contact.php">Contact</a>
-            </div>
-            <div class="nav-actions">
-                <button id="loginBtn" class="btn-login">
-                    <i class="fas fa-user"></i>
-					<a href="/connexion.php">Se connecter</a>
-				</button>
-			</div>
-		</nav>
-	</header>
-
     <div class="navigation-buttons">
         <a href="/contact.php" class="nav-btn back-btn">
             <i class="fas fa-arrow-left"></i>
--- config/config.php
+++ config/config.php
@@ -6,16 +6,16 @@ define('DB_USER', 'vm231606');  // Nom d'utilisateur de la base de données
 define('DB_PASS', 'dogman');  // Mot de passe de la base de données
 
 
-
+/*
 define('DB_NAME', 'cn230854');  // Nom de la base de données
 define('DB_USER', 'cn230854');  // Nom d'utilisateur de la base de données
 define('DB_PASS', '28022005');  // Mot de passe de la base de données
+*/
 
-/*
 define('DB_NAME', 'lk231825');  // Nom de la base de données
 define('DB_USER', 'lk231825');  // Nom d'utilisateur de la base de données
 define('DB_PASS', 'pirate1708');  // Mot de passe de la base de données
-*/
+
 
 
 define('DB_PORT', '5432');  // Mot de passe de la base de données
