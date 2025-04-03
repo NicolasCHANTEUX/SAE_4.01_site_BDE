85dc8af77f5fefc4130202b5e10ee6dc51fe2d0b
Nicolas CHANTEUX <nicolas.chanteux@etu.univ-lehavre.fr>
2025-04-03 09:47:55 +0200

    ajout bouton Panier dans la page Boutique

--- app/views/boutique/boutique.php
+++ app/views/boutique/boutique.php
@@ -2,6 +2,19 @@
 require_once 'app/views/template/header.php';
 ?>
 	<link rel="stylesheet" href="/assets/css/boutique.css">
+	<link rel="stylesheet" href="/assets/css/produit.css">
+
+	<div class="navigation-buttons">
+		<a href="/" class="nav-btn back-btn">
+			<i class="fas fa-arrow-left"></i>
+			<span>Accueil</span>
+		</a>
+		<a href="/panier.php" class="nav-btn cart-btn">
+			<span>Panier</span>
+			<i class="fas fa-shopping-cart"></i>
+		</a>
+	</div>
+
 	<main id="app">
 			<div class="container">
 				<div id="boutique-app" class="boutique-container">
