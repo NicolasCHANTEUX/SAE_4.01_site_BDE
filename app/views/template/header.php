2c323d5e26bb7faf42bc21d721b417309810870f
Kylian LucasThuillier <kylian.lucast@gmail.com>
2025-04-03 10:30:21 +0200

    Tout il est bon

--- app/views/template/header.php
+++ app/views/template/header.php
@@ -5,6 +5,26 @@ if(session_status() == PHP_SESSION_NONE) {
 require_once 'app/middlewares/AuthMiddleware.php';
 ?>
 
+<?php
+
+$isAdmin = isset($_SESSION['user']) && $_SESSION['user']['role'] === 'admin';
+
+ // Détermine la page actuelle
+ $currentPage = basename($_SERVER['REQUEST_URI']);
+
+ // Associe chaque page à son équivalent admin
+$adminPages = [
+    'index.php' => 'accueilAdmin.php',
+    'evenement.php' => 'evenementAdmin.php',
+    'boutique.php' => 'boutiqueAdmin.php',
+    'contact.php' => 'contactAdmin.php',
+    'connexion.php' => 'adminConnexion.php'
+];
+
+// Définit le lien de redirection pour le bouton Mode Édition
+$adminPageLink = $adminPages[$currentPage] ?? 'accueilAdmin.php'; // Par défaut, redirige vers admin.php
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -49,6 +69,12 @@ require_once 'app/middlewares/AuthMiddleware.php';
         <!-- Menu PC -->
         <nav class="main-nav">
             <div class="nav-brand">
+                
+                
+                <?php if ($isAdmin): ?>
+                    <button onclick="window.location.href='<?= htmlspecialchars($adminPageLink) ?>'" class="mode-edition-btn">Mode Édition</button>
+                <?php endif; ?>
+
                 <a href="/">
                     <img src="/assets/images/logo.png" alt="Logo BDE" class="logo">
                     <span>BDE Info</span>
--- config/config.php
+++ config/config.php
@@ -6,10 +6,11 @@ define('DB_USER', 'vm231606');  // Nom d'utilisateur de la base de données
 define('DB_PASS', 'dogman');  // Mot de passe de la base de données
 */
 
+/*
 define('DB_NAME', 'cn230854');  // Nom de la base de données
 define('DB_USER', 'cn230854');  // Nom d'utilisateur de la base de données
 define('DB_PASS', '28022005');  // Mot de passe de la base de données
-
+*/
 
 define('DB_NAME', 'lk231825');  // Nom de la base de données
 define('DB_USER', 'lk231825');  // Nom d'utilisateur de la base de données
