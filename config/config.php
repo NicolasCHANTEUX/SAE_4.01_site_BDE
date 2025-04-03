4ddd553dab7413592f77b868c002cc35a78c6584
Nicolas CHANTEUX <nicolas.chanteux@etu.univ-lehavre.fr>
2025-04-03 12:53:17 +0200

    photo event poker

--- config/config.php
+++ config/config.php
@@ -1,10 +1,10 @@
 <?php
 // Définition de constantes pour la configuration de la base de données
 define('DB_HOST', 'woody');  // Hôte de la base de données (généralement localhost)
-define('DB_NAME', 'vm231606');  // Nom de la base de données
+/*define('DB_NAME', 'vm231606');  // Nom de la base de données
 define('DB_USER', 'vm231606');  // Nom d'utilisateur de la base de données
 define('DB_PASS', 'dogman');  // Mot de passe de la base de données
-
+*/
 
 /*
 define('DB_NAME', 'cn230854');  // Nom de la base de données
